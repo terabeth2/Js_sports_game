48d3bdf510206c2f802b1b524d9987ab3e28ab07
terabeth2 <terabeth@me.com>
2020-05-06 22:23:36 -0500

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+const teamOneButton = document.querySelector("#teamone-shoot-button");
+teamOneButton.addEventListener("click", function () {
+    console.log("Team one button press");
+})
+
+
+const teamTwoButton = document.querySelector("#teamtwo-shoot-button");
+teamTwoButton.addEventListener("click", function () {
+    console.log("Team Two button press");
+})
